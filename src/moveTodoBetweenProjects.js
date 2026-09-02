2e74466818ced0b9bd54f583be7de3b7bf749a71
whyitboi <20414206+whyitboi@users.noreply.github.com>
2026-09-02 16:51:38 +0300

    Corrected the mistake of using indexOf() instead of findIndex()

--- src/moveTodoBetweenProjects.js
+++ src/moveTodoBetweenProjects.js
@@ -2,19 +2,11 @@
 export const moveTodo = (projectFrom, projectTo, todoListTitle) => {
   projectFrom.todoLists.forEach((todo) => {
     if (todo.title === todoListTitle) {
-      const index = projectFrom.todoLists.indexOf((todo) => {
-        console.log(index);
-        //return todo.title === todoListTitle
+      const index = projectFrom.todoLists.findIndex((todo) => {
+        return todo.title === todoListTitle;
       });
       console.log("I like to move it move it");
-
-      //getting -1 here. I am trying to retireve the index of where I find this title
-      //So i can splice copy and push it to the projectTo.todoLists array
-      //and pop it from the projectFrtom.todoLists array
-
-      console.log(projectFrom.todoLists.indexOf(todoListTitle));
-      //console.log(index);
-      //projectFrom.todoLists.splice(todo.indexOf(todoListTitle))
+      console.log(index);
     } else {
       console.log("I in fact do not like to move it move it");
       return;
